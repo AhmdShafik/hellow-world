1d8b54484eaac6332eec287b686017fcc98080f1
AhmedShafik <aboshafik60@gmail.com>
2022-02-10 16:14:03 +0300

    Commit

---
+++ src/Components/Advanced/Refs/FocusInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class FocusInput extends Component {
+  render() {
+    return (
+      <div>FocusInput</div>
+    )
+  }
+}
+
+export default FocusInput
