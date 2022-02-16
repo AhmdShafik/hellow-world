49be45a3b14f0afe204bfe4bc5b8cbd3d3fbf106
AhmedShafik <aboshafik60@gmail.com>
2022-02-16 13:39:05 +0300

    Advanced React

---
+++ src/Components/Advanced/Context/ComponentC.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ComponentC extends Component {
+  render() {
+    return (
+      <div>ComponentC</div>
+    )
+  }
+}
+
+export default ComponentC
